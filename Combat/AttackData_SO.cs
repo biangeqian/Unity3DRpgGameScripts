04e0642587e6708f9d4b8760b9cf60964b825776
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-03-16 13:36:54 +0800

    Add files via upload

---
+++ Combat/AttackData_SO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Attack",menuName = "Attack/Attack Data")]
+public class AttackData_SO : ScriptableObject
+{
+    //攻击距离
+    public float attackRange;
+    //技能距离
+    public float skillRange;
+    //CD
+    public float coolDown;
+    //最小攻击
+    public int minDamage;
+    //最大攻击
+    public int maxDamage;
+    //爆伤
+    public float criticalMultiplier;
+    //暴击率
+    public float criticalChance;
+
+}
