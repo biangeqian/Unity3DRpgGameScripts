5e78939ced97a0f3e471d66d74ac0718ba7abf12
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-03-16 13:33:55 +0800

    Add files via upload

---
+++ Character Stats/ScriptableObject/CharacterData_SO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Data",menuName="Character Stats/Data" )]
+public class CharacterData_SO : ScriptableObject
+{
+    [Header("Stats Info")]
+    //最大血量
+    public int maxHealth;
+    //当前血量
+    public int currentHealth;
+    //基础防御
+    public int baseDefence;
+    //当前防御
+    public int currentDefence;
+}
