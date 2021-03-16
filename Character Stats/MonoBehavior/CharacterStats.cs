bcfc26cf41bd25dde6a834fde8a7699f1508f0bc
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-03-16 13:32:58 +0800

    Add files via upload

---
+++ Character Stats/MonoBehavior/CharacterStats.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    //模板数据
+    public CharacterData_SO templateData;
+
+    public CharacterData_SO characterData;
+    public AttackData_SO attackData;
+    [HideInInspector]
+    public bool isCritical;
+
+    void Awake()
+    {
+        if (templateData != null)
+        {
+            characterData = Instantiate(templateData);
+        }
+    }
+
+    #region Read from Data_SO
+    public int MaxHealth
+    {
+        get{if (characterData != null)return characterData.maxHealth;else return 0;}
+        set{characterData.maxHealth = value;}
+    }
+    public int CurrentHealth
+    {
+        get { if (characterData != null) return characterData.currentHealth; else return 0; }
+        set { characterData.currentHealth = value; }
+    }
+    public int BaseDefence
+    {
+        get { if (characterData != null) return characterData.baseDefence; else return 0; }
+        set { characterData.baseDefence = value; }
+    }
+    public int CurrentDefence
+    {
+        get { if (characterData != null) return characterData.currentDefence; else return 0; }
+        set { characterData.currentDefence = value; }
+    }
+    #endregion
+
+    #region Character Combat
+    //受到攻击
+    public void TakeDamage(CharacterStats attacker,CharacterStats defender)
+    {
+        int damage = Mathf.Max(attacker.CurrentDamage() - defender.CurrentDefence,0);
+        CurrentHealth = Mathf.Max(CurrentHealth - damage, 0);
+        //暴击时触发挨打的人的受击动画
+        if (attacker.isCritical)
+        {
+            defender.GetComponent<Animator>().SetTrigger("Hit");
+        }
+
+        //更新血量UI
+
+        //结算经验值
+
+    }
+    private int CurrentDamage()
+    {
+        float coreDamage = Random.Range(attackData.minDamage, attackData.maxDamage);
+        if (isCritical)
+        {
+            coreDamage *= attackData.criticalMultiplier;
+            Debug.Log("暴击!" + coreDamage);
+        }
+        return (int)coreDamage;
+    }
+    #endregion
+}
