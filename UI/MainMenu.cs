3d6255d310a69c9017cb49eb8824b5188c93a2ab
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-03-23 23:27:21 +0800

    Add files via upload

---
+++ UI/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    Button newGameBtn;
+    Button continueBtn;
+    Button quitBtn;
+
+    void Awake()
+    {
+        newGameBtn = transform.GetChild(1).GetComponent<Button>();
+        continueBtn = transform.GetChild(2).GetComponent<Button>();
+        quitBtn = transform.GetChild(3).GetComponent<Button>();
+
+        newGameBtn.onClick.AddListener(NewGame);
+        continueBtn.onClick.AddListener(ContinueGame);
+        quitBtn.onClick.AddListener(QuitGame);
+    }
+    void NewGame()
+    {
+        PlayerPrefs.DeleteAll();
+        //转换场景
+        SceneController.Instance.TransitionToFirstLevel();
+    }
+    void ContinueGame()
+    {
+        //转换场景,读取进度
+        SceneController.Instance.TransitionToLoadGame();
+    }
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
