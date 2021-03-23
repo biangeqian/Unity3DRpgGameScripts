afa49f36f6f37fc5b9dccf6ae0e797ba0be7a6ae
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-03-23 23:29:28 +0800

    Add files via upload

---
+++ Transition/SceneController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : Singleton<SceneController>
+{
+    public GameObject playerPrefeb;
+    GameObject player;
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(this);
+    }
+
+    public void TransitionToDestination(TransitionPoint transitionPoint)
+    {
+        switch (transitionPoint.transitionType)
+        {
+            case TransitionPoint.TransitionType.SameScene:
+                StartCoroutine(Transition(SceneManager.GetActiveScene().name, transitionPoint.destinationTag));               
+                break;
+            case TransitionPoint.TransitionType.DifferentScene:
+                StartCoroutine(Transition(transitionPoint.sceneName, transitionPoint.destinationTag));
+                break;
+        }
+    }
+    IEnumerator Transition(string sceneName,TransitionDestination.DestinationTag destinationTag)
+    {
+        //保存数据
+        SaveManager.Instance.SavePlayerData();
+        if (SceneManager.GetActiveScene().name != sceneName)
+        {
+            //异步加载
+            yield return SceneManager.LoadSceneAsync(sceneName);
+            yield return Instantiate(playerPrefeb,GetDestination(destinationTag).transform.position, GetDestination(destinationTag).transform.rotation);
+            //读取数据(这里实际上和人物自己脚本上的读取重复了,不过问题不大)
+            SaveManager.Instance.LoadPlayerData();
+            yield break;
+        }
+        else
+        {
+            player = GameManager.Instance.playerStats.gameObject;
+            player.GetComponent<CharacterController>().Move(GetDestination(destinationTag).transform.position - player.transform.position);
+            player.GetComponent<CharacterController>().Move(Vector3.zero);
+            yield return null;
+        }  
+    }
+    private TransitionDestination GetDestination(TransitionDestination.DestinationTag destinationTag)
+    {
+        var entrances = FindObjectsOfType<TransitionDestination>();
+        for(int i=0;i<entrances.Length;i++)
+        {
+            if (entrances[i].destinationTag == destinationTag)
+            {
+                return entrances[i];
+            }
+        }
+        return null;
+    }
+    public void TransitionToMenu()
+    {
+        StartCoroutine(LoadMenu());
+    }
+    public void TransitionToLoadGame()
+    {
+        StartCoroutine(LoadLevel(SaveManager.Instance.SceneName));
+    }
+    public void TransitionToFirstLevel()
+    {
+        StartCoroutine(LoadLevel("SampleScene"));
+    }
+    IEnumerator LoadLevel(string scene)
+    {
+        if (scene != "")
+        {
+            yield return SceneManager.LoadSceneAsync(scene);
+            yield return player = Instantiate(playerPrefeb, GameManager.Instance.GetEntrance().position, GameManager.Instance.GetEntrance().rotation);
+            //保存
+            SaveManager.Instance.SavePlayerData();
+            yield break;
+        }  
+    }
+    IEnumerator LoadMenu()
+    {
+        if (SceneManager.GetActiveScene().name != "Menu")
+        {
+            SaveManager.Instance.SavePlayerData();
+            yield return SceneManager.LoadSceneAsync("Menu");
+            yield break;
+        }
+        yield break;
+    }
+}
---
+++ Transition/TransitionPoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TransitionPoint : MonoBehaviour
+{
+    public enum TransitionType
+    {
+        SameScene,
+        DifferentScene
+    }
+
+    [Header("Transition Info")]
+    public string sceneName;
+    public TransitionType transitionType;
+    public TransitionDestination.DestinationTag destinationTag;
+
+    private bool canTrans;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canTrans = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canTrans = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame &&canTrans)
+        {
+            //Debug.Log("传送");
+            //传送
+            SceneController.Instance.TransitionToDestination(this);
+        }
+    }
+}
