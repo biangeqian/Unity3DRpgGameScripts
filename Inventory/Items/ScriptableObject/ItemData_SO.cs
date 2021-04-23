40b44ee69699fd936163dbd69b26fdf9093bd7b6
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-04-23 14:49:39 +0800

    Add files via upload

---
+++ Inventory/Items/ScriptableObject/ItemData_SO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType { Useable,Weapon,Armor}
+[CreateAssetMenu(fileName ="New Item",menuName ="Inventory/Item Data")]
+public class ItemData_SO : ScriptableObject
+{
+    public ItemType itemType;
+    public string itemName;
+    public Sprite itemIcon;//图标
+    public int itemAmount;//本身堆叠的数量
+    [TextArea]
+    public string description = "";//物品描述
+    public bool stackable;//可堆叠
+
+    [Header("Useable")]
+    public UseableItemData_SO useableData;
+    [Header("Weapon")]
+    public GameObject weaponPrefab;
+    public AttackData_SO weaponData;
+}
