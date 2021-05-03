fc301fb396f1413eec76a267a203113ab0d021ce
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-05-03 22:09:15 +0800

    Add files via upload

--- Characters/PlayerControllerWASD.cs
+++ Characters/PlayerControllerWASD.cs
@@ -11,7 +11,7 @@ public class PlayerControllerWASD : MonoBehaviour
     Vector2 movementInput;
     Vector2 cameraInput;
     //访问按键
-    public InputElement northInput = new InputElement();
+    //public InputElement northInput = new InputElement();
 
     [Header("Player Camera Data")]
     public Transform cameraSystem;
@@ -28,6 +28,7 @@ public class PlayerControllerWASD : MonoBehaviour
     public float cameraMaxAngle;
     public float cameraMinAngle;
     public Vector2 cameraAngles;
+    Vector3 newPosition= new Vector3(0, 3, -20);
 
     [Header("Player Movement Data")]
     private CharacterController controller;
@@ -36,15 +37,22 @@ public class PlayerControllerWASD : MonoBehaviour
     public float rotationSpeed;
     [Range(0, 10)]
     public float movementSpeed;
-    public bool isThirdPerson;
+    public bool isThirdPerson=true;
     public float jumpSpeed;
-    public float gravity;
+    public float gravity=9.8f;
+    private bool IsGrounded=true;
+    private bool ReadyToJump=true;
+    //垂直速度
+    float VerticalSpeed=-1f;
 
     private Animator anim;
     private CharacterStats characterStats;
     private float lastAttackTime;
     private bool isDead;
     public bool MouseLeftClicked = false;
+    public bool isDizzy;
+    public bool ObserverFlag;//是否已广播死亡
+    
 
     void Awake()
     {
@@ -66,9 +74,9 @@ public class PlayerControllerWASD : MonoBehaviour
             inputController.Movement.Move.performed += inputController => movementInput = inputController.ReadValue<Vector2>();
             inputController.Movement.Camera.performed += inputController => cameraInput = inputController.ReadValue<Vector2>();
 
-            inputController.Actions.ChangeView.started += inputController => northInput.risingEdge = true;
-            inputController.Actions.ChangeView.performed += inputController => northInput.longPress = true;
-            inputController.Actions.ChangeView.canceled += inputController => northInput.releaseEdges();
+            //inputController.Actions.ChangeView.started += inputController => northInput.risingEdge = true;
+            //inputController.Actions.ChangeView.performed += inputController => northInput.longPress = true;
+            //inputController.Actions.ChangeView.canceled += inputController => northInput.releaseEdges();
 
             inputController.Attack.Attack1.performed += inputController => MouseLeftClicked = true;
             inputController.Attack.Attack1.canceled += inputController => MouseLeftClicked = false;
@@ -82,37 +90,46 @@ public class PlayerControllerWASD : MonoBehaviour
     #endregion
     void Start()
     {
+        isThirdPerson=true;//不写就成false了,但是初始化的明明是true,不知道为什么,离谱
         SaveManager.Instance.LoadPlayerData();
     }
     void Update()
     {
+        //Debug.Log(isThirdPerson);
+
         isDead = characterStats.CurrentHealth == 0;
-        if (isDead)
+        if (isDead&&!ObserverFlag)
         {
             //广播角色的死亡
             GameManager.Instance.NotifyObservers();
-            //TODO:死亡时禁用玩家输入
+            ObserverFlag = true;
+        }
+        else
+        {
+            //眩晕时也要计算重力
+            if (!isDizzy)
+            {
+                if (controller.enabled == true)
+                {
+                    //处理运动
+                    HandleMovement();
+                    //处理旋转的方式
+                    MovementRotation();
+                    if (MouseLeftClicked)
+                    {
+                        Attack();
+                    }
+                }
+            }
+            else
+            {
+                //TODO:眩晕时也要计算重力
+                controller.Move(Vector3.zero);
+            }
         }
         SwitchAnimation();
         lastAttackTime -= Time.deltaTime;
-
-        //处理运动
-        HandleMovement();
-        //处理旋转的方式
-        MovementRotation();
-        //character follows camera rotation
-        //character follows camera and input
-        //focus/lock on camera
-        //相机切换角度,一三人称切换的时候用
-        CameraChangeView();
-        //相机移动
-        CameraMovement();
-
-        if (MouseLeftClicked)
-        {
-            //Debug.Log("攻击");
-            Attack();
-        }
+    
     }
 
     private void SwitchAnimation()
@@ -127,26 +144,64 @@ public class PlayerControllerWASD : MonoBehaviour
     {
         moveDirection = cameraObject.forward * movementInput.y;
         moveDirection += cameraObject.right * movementInput.x;
-        Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
-        projectedVelocity.Normalize();
-        projectedVelocity *= movementSpeed;
+        //水平速度方向
+        Vector3 HorizontalVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+        HorizontalVelocity.Normalize();
+        //水平速度
+        HorizontalVelocity *= movementSpeed;
+        
 
-        projectedVelocity.y = controller.velocity.y;
-
-        if (controller.isGrounded)
+        if(!Keyboard.current.spaceKey.wasPressedThisFrame&&IsGrounded)
         {
-            projectedVelocity.y = 0;
-            if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            ReadyToJump=true;
+        }
+        if(IsGrounded)
+        {
+            //垂直速度
+            VerticalSpeed=gravity*0.3f*-1;
+            if(Keyboard.current.spaceKey.wasPressedThisFrame&&ReadyToJump)
             {
-                projectedVelocity += Vector3.up * jumpSpeed;
+                VerticalSpeed=jumpSpeed;
+                IsGrounded=false;
+                ReadyToJump=false;
             }
         }
         else
         {
-            projectedVelocity += Vector3.down * gravity*Time.deltaTime;
+            if(!Keyboard.current.spaceKey.wasPressedThisFrame&&VerticalSpeed>0.0f)
+            {
+                VerticalSpeed-=10f*Time.deltaTime;
+            }
+            if(Mathf.Approximately(VerticalSpeed,0f))
+            {
+                VerticalSpeed=0f;
+            }
+            //在空中则施加重力
+            VerticalSpeed-=gravity*Time.deltaTime;
         }
-        controller.Move(projectedVelocity * Time.deltaTime);//Move不考虑钟离
-        //controller.SimpleMove(projectedVelocity);
+        
+        if(IsGrounded)
+        {
+            RaycastHit hit;
+            Ray ray =new Ray(transform.position+Vector3.up*0.5f,Vector3.down);
+            if(Physics.Raycast(ray,out hit,1f,Physics.AllLayers,QueryTriggerInteraction.Ignore))
+            {
+                HorizontalVelocity=Vector3.ProjectOnPlane(HorizontalVelocity, hit.normal);
+            }
+            else
+            {
+                Debug.Log("不会吧不会吧,不会真能else吧");
+            }
+        }
+        else
+        {
+            HorizontalVelocity=movementSpeed*transform.forward;
+        }
+
+        Vector3 movement=HorizontalVelocity*Time.deltaTime;;
+        movement+=VerticalSpeed*Vector3.up*Time.deltaTime;
+        controller.Move(movement);   
+        IsGrounded=controller.isGrounded;
     }
     //角色转向
     private void MovementRotation()
@@ -199,20 +254,23 @@ public class PlayerControllerWASD : MonoBehaviour
     private void CameraChangeView()
     {
         //这里是长按v时有一个类似刺客信条鹰眼的第三人称窥视效果,也可以改成和英灵殿一样的短按长久切换
-        if (northInput.fallingEdge)//第一人称
+        //if (northInput.fallingEdge)//第一人称
+        if(Keyboard.current.vKey.wasPressedThisFrame)
         {
-            isThirdPerson = false;
-            Vector3 newPosition = new Vector3(0, 1.03f, 0.16f);
-            //cameraPivot.localPosition = Vector3.Lerp(cameraPivot.localPosition, newPosition, Time.deltaTime * cameraFollowSpeed * 2f);
-            cameraObject.localPosition = Vector3.Lerp(cameraObject.localPosition, newPosition, Time.deltaTime * cameraFollowSpeed * 2f);
+            if(isThirdPerson== true)
+            {
+                isThirdPerson = false; 
+                newPosition = new Vector3(0, 1.03f, 0.16f);
+            }
+            else
+            {
+                isThirdPerson = true;
+                newPosition = new Vector3(0, 3, -20);
+            }
+            
         }
-        else//第三人称
-        {
-            isThirdPerson = true;
-            Vector3 newPosition = new Vector3(0, 3, -20);
-            //cameraPivot.localPosition = Vector3.Lerp(cameraPivot.localPosition, newPosition, Time.deltaTime * cameraFollowSpeed*2f);
-            cameraObject.localPosition = Vector3.Lerp(cameraObject.localPosition, newPosition, Time.deltaTime * cameraFollowSpeed * 2f);
-        }
+        //cameraPivot.localPosition = Vector3.Lerp(cameraPivot.localPosition, newPosition, Time.deltaTime * cameraFollowSpeed*2f);
+        cameraObject.localPosition = Vector3.Lerp(cameraObject.localPosition, newPosition, Time.deltaTime * cameraFollowSpeed * 2f);
     }
 
     private void Attack()
@@ -255,15 +313,32 @@ public class PlayerControllerWASD : MonoBehaviour
             }
         }     
     }
-
-    //publc bool wasGround()
-    //{
-     //   var colliders=controller.c
-    //}
-
     private void LateUpdate()
     {
-        //长按方案的时候用
-        //northInput.resetEdges();
+        //相机切换角度,一三人称切换的时候用
+        CameraChangeView();
+        //相机移动
+        CameraMovement();
     }
+
+    private bool WasGround(){
+        float radius=controller.radius*0.9f;
+        float overLapCapsuleOffset=0.2f;
+        Vector3 pointBottom = transform.position + transform.up * radius-transform.up*overLapCapsuleOffset;
+        Vector3 pointTop = transform.position + transform.up * controller.height - transform.up * radius;
+        LayerMask ignoreMask = ~LayerMask.GetMask("Player");
+ 
+        Collider[] colliders = Physics.OverlapCapsule(pointBottom, pointTop, radius, ignoreMask);
+        Debug.DrawLine(pointBottom, pointTop,Color.green);
+        if (colliders.Length!=0)
+        {
+            return true;
+        }
+        else
+        {
+
+            return false;
+        }
+    }
+
 }
