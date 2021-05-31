befd68760edd45894af83e34e058ee548822a5cd
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-05-31 23:47:10 +0800

    Add files via upload

---
+++ Dialogue/UI/DialogueUI.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DialogueUI : Singleton<DialogueUI>
+{
+    [Header("Basic Elements")]
+    public Image icon;
+    public Text mainText;
+    public Button nextButton;
+    public GameObject dialoguePanel;
+    [Header("Options")]
+    public RectTransform optionPanel;
+    public OptionUI optionPrefeb;
+
+    [Header("Data")]
+    public DialogueData_SO currentData;
+    int currentIndex=0;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        nextButton.onClick.AddListener(ContinueDialogue);
+    }
+    void ContinueDialogue()
+    {
+        if(currentIndex<currentData.dialoguePieces.Count)
+            UpdateMainDialogue(currentData.dialoguePieces[currentIndex]);
+        else
+            dialoguePanel.SetActive(false);
+    }
+    public void UpdateDialogueData(DialogueData_SO data)
+    {
+        currentData=data;
+        currentIndex=0;
+    }
+    public void UpdateMainDialogue(DialoguePiece piece)
+    {
+        dialoguePanel.SetActive(true);
+        currentIndex++;
+        
+        if(piece.image!=null)
+        {
+            icon.enabled=true;
+            icon.sprite=piece.image;
+        }
+        else
+        {
+            icon.enabled=false;
+        }
+
+        mainText.text="";
+        mainText.text=piece.text;
+        //mainText.DOText(piece.text,1f);
+        
+
+        if(piece.options.Count==0&&currentData.dialoguePieces.Count>0)
+        {
+            nextButton.interactable=true;
+            nextButton.gameObject.SetActive(true);
+            nextButton.transform.GetChild(0).gameObject.SetActive(true);
+            
+        }
+        else
+        {
+            nextButton.interactable=false;
+            nextButton.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        //创建options
+        CreateOptions(piece);
+    }
+    void CreateOptions(DialoguePiece piece)
+    {
+        if(optionPanel.childCount>0)
+        {
+            for(int i=0;i<optionPanel.childCount;i++)
+            {
+                Destroy(optionPanel.GetChild(i).gameObject);
+            }
+        }
+        for(int i=0;i<piece.options.Count;i++)
+        {
+            var option=Instantiate(optionPrefeb,optionPanel);
+            option.UpdateOption(piece,piece.options[i]);
+        }
+    }
+}
---
+++ Dialogue/UI/OptionUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionUI : MonoBehaviour
+{
+    public Text optionText;
+    private Button thisButton;
+    private DialoguePiece currentPiece;
+    private string nextPieceID;
+    private bool takeQuest;
+    void Awake() 
+    {
+        thisButton=GetComponent<Button>();
+        thisButton.onClick.AddListener(OnOptionClicked);
+    }
+    public void UpdateOption(DialoguePiece piece,DialogueOption option) 
+    {
+        currentPiece=piece;
+        optionText.text=option.text;
+        nextPieceID=option.targetID;
+        takeQuest=option.takeQuest;
+    }
+    public void OnOptionClicked()
+    {
+        if(currentPiece.quest!=null)
+        {
+            var newTask=new QuestManager.QuestTask
+            {
+                questData=Instantiate(currentPiece.quest)
+            };
+            if(takeQuest)//接受任务
+            {
+                //添加到任务列表
+                //判断任务是否已经存在
+                if(QuestManager.Instance.HaveQuest(newTask.questData))
+                {
+                    //判断是否完成,给予奖励
+
+                }
+                else
+                {
+                    QuestManager.Instance.tasks.Add(newTask);
+                    QuestManager.Instance.GetTask(newTask.questData).IsStarted=true;
+                }
+            }
+        }
+        if(nextPieceID=="")
+        {
+            DialogueUI.Instance.dialoguePanel.SetActive(false);
+            return;
+        }
+        else
+        {
+            DialogueUI.Instance.UpdateMainDialogue(DialogueUI.Instance.currentData.dialogueIndex[nextPieceID]);
+        }
+    }
+}
