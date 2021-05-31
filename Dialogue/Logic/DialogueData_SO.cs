cf8f99b7bf70d9dfd17099519637b089020e08a3
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-05-31 23:46:05 +0800

    Add files via upload

---
+++ Dialogue/Logic/DialogueData_SO.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Dialogue",menuName ="Dialogue/Dialogue Data")]
+public class DialogueData_SO : ScriptableObject
+{
+    public List<DialoguePiece> dialoguePieces=new List<DialoguePiece>();
+    public Dictionary<string,DialoguePiece> dialogueIndex=new Dictionary<string, DialoguePiece>();
+    //仅在编辑器中执行
+#if UNITY_EDITOR
+    //数据发生改变时执行
+    void OnValidate() 
+    {
+        dialogueIndex.Clear();
+        foreach(var piece in dialoguePieces)
+        {
+            if(!dialogueIndex.ContainsKey(piece.ID))
+            {
+                dialogueIndex.Add(piece.ID,piece);
+            }
+        }
+    }
+#else
+    void Awake() 
+    {
+        dialogueIndex.Clear();
+        foreach(var piece in dialoguePieces)
+        {
+            if(!dialogueIndex.ContainsKey(piece.ID))
+            {
+                dialogueIndex.Add(piece.ID,piece);
+            }
+        }
+    }
+#endif
+}
