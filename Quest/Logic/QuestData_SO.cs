5e7f549d312e1ebdcd00a476f6a350bc9873eb1f
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-05-31 23:48:39 +0800

    Add files via upload

---
+++ Quest/Logic/QuestData_SO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Quest",menuName ="Quest/Quest Data")]
+public class QuestData_SO : ScriptableObject
+{
+    [System.Serializable]
+    public class QuestRequire
+    {
+        public string name;//物品名或怪物名
+        public int requireAmount;//需求数量
+        public int currentAmount;//当前已完成属性
+    }
+    public string questName;//任务名,唯一
+    [TextArea]
+    public string description;
+    public bool isStarted;
+    public bool isComplete;
+    public bool isFinished;//不可重复接取
+    
+    public List<QuestRequire> questRequires=new List<QuestRequire>();
+}
