766aa93dd84645ae053976eb37b2739886813065
biangeqian <55963921+biangeqian@users.noreply.github.com>
2021-06-04 22:46:39 +0800

    Add files via upload

--- Quest/Logic/QuestData_SO.cs
+++ Quest/Logic/QuestData_SO.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System.Net.NetworkInformation;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(fileName ="New Quest",menuName ="Quest/Quest Data")]
 public class QuestData_SO : ScriptableObject
@@ -20,4 +22,63 @@ public class QuestData_SO : ScriptableObject
     public bool isFinished;//不可重复接取
     
     public List<QuestRequire> questRequires=new List<QuestRequire>();
+    public List<InventoryItem> rewards=new List<InventoryItem>();
+
+    public void CheckQuestProcess()
+    {
+        var finishRequires=questRequires.Where(r=>r.requireAmount<=r.currentAmount);
+        isComplete=finishRequires.Count()==questRequires.Count;
+
+        if(isComplete)
+        {
+            Debug.Log("任务完成");
+        }
+    }
+    public void GiveRewards()
+    {
+        foreach(var reward in rewards)
+        {
+            if(reward.amount<0)
+            {
+                int requireCount=Mathf.Abs(reward.amount);
+                if(InventoryManager.Instance.QuestItemInBag(reward.itemData)!=null)
+                {
+                    if(InventoryManager.Instance.QuestItemInBag(reward.itemData).amount<=requireCount)
+                    {
+                        requireCount-=InventoryManager.Instance.QuestItemInBag(reward.itemData).amount;
+                        InventoryManager.Instance.QuestItemInBag(reward.itemData).amount=0;
+
+                        if(InventoryManager.Instance.QuestItemInAction(reward.itemData)!=null)
+                        {
+                            InventoryManager.Instance.QuestItemInAction(reward.itemData).amount-=requireCount;
+                        }
+                    }
+                    else
+                    {
+                        InventoryManager.Instance.QuestItemInBag(reward.itemData).amount-=requireCount;
+                    }
+                }
+                else
+                {
+                    InventoryManager.Instance.QuestItemInAction(reward.itemData).amount-=requireCount;
+                }
+            }
+            else
+            {
+                InventoryManager.Instance.inventoryData.AddItem(reward.itemData,reward.amount);
+            }
+            InventoryManager.Instance.inventoryUI.RefreshUI();
+            InventoryManager.Instance.actionUI.RefreshUI();
+        }
+    }
+    //当前任务需求的所有东西
+    public List<string> RequireTargetName()
+    {
+        List<string> targetNameList=new List<string>();
+        foreach(var require in questRequires)
+        {
+            targetNameList.Add(require.name);
+        }
+        return targetNameList;
+    }
 }
--- Quest/Logic/QuestManager.cs
+++ Quest/Logic/QuestManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Threading.Tasks;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -15,6 +16,47 @@ public class QuestManager : Singleton<QuestManager>
 
     }
     public List<QuestTask> tasks=new List<QuestTask>();
+
+    void Start() 
+    {
+        LoadQuestManager();
+    }
+    public void SaveQuestManager()
+    {
+        PlayerPrefs.SetInt("QuestCount",tasks.Count);
+        for(int i=0;i<tasks.Count;i++)
+        {
+            SaveManager.Instance.Save(tasks[i].questData,"task"+i);
+        }
+        Debug.Log("保存");
+    }
+    public void LoadQuestManager()
+    {
+        var questCount=PlayerPrefs.GetInt("QuestTask");
+        for(int i=0;i<questCount;i++)
+        {
+            var newQuest=ScriptableObject.CreateInstance<QuestData_SO>();
+            SaveManager.Instance.Load(newQuest,"task"+i);
+            tasks.Add(new QuestTask{questData=newQuest});
+        }
+        Debug.Log("加载");
+    }
+
+    //敌人死亡/拾取物品时调用
+    public void UpdateQuestProgress(string requirename,int amount)
+    {
+        foreach(var task in tasks)
+        {
+            if(task.IsFinished) continue;
+            var matchTask=task.questData.questRequires.Find(r=>r.name==requirename);
+            if(matchTask!=null)
+            {
+                matchTask.currentAmount+=amount;
+                task.questData.CheckQuestProcess();
+            }  
+        }
+    }
+
     public bool HaveQuest(QuestData_SO data)
     {
         if(data!=null)
